513078262a0a42847e422669bbfc77880b8d5734
Rich Boakes <rjb@port.ac.uk>
2023-12-02 10:52:16 +0000

    Discoverd some empty files (124, 125, 126, 127, 458 and 459) so have built a guard against that happening again, redownloaded and reprocessed

--- combine.js
+++ combine.js
@@ -9,21 +9,32 @@ async function combine() {
   const files = await fs.readdir(inPath);
   const csoFiles = files.filter(file => /^CSO-\d+\.json$/.test(file));
 
+  let runningTotal = 0;
+  let fileCount = 0;
+
   const result = { pages: [], totalPages: 0, totalItems: 0, items: [] };
 
   for (const file of csoFiles) {
     const filePath = path.join(inPath, file);
     const data = JSON.parse(await fs.readFile(filePath, 'utf-8'));
 
-    result.items.push(...data.items);
-    result.pages.push(data.currentPage);
+    result.items.push(...data.items); // add all teh items in the current file to the result
+    result.pages.push(data.currentPage); // include the current page number in the results
     result.totalItems = Number(data.totalItems);
     result.totalPages = Number(data.totalPages);
+
+    fileCount++;
+    runningTotal += data.items.length;
+    if (runningTotal !== fileCount * 25) {
+      // we expect 25 entries per file so check that the running total matches the number of files
+      log( typeof runningTotal, typeof result.items.length);
+      throw new Error(`❌ Totals have diverged.  Expected ${runningTotal} items, but found ${result.items.length} when processing #${fileCount} ${file}.`);
+    }
   }
 
   if (result.items.length !== result.totalItems) {
     log( typeof result.items.length, typeof result.totalItems);
-    throw new Error(`❌ Expected ${result.items.length} items, but found ${result.totalItems}`);
+    throw new Error(`❌ Expected ${result.totalItems} items, but found ${result.items.length}`);
   }
 
   if (result.pages.length !== result.totalPages) {
